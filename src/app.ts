18c2c002b31b9079fd4f9b59e9e596b63168f374
Guillaume Gomez <guillaume.gomez846@gmail.com>
2020-10-30 00:37:33 +0100

    add grey scale function

--- src/app.ts
+++ src/app.ts
@@ -14,9 +14,36 @@ fileInput.onchange = (e: Event) => {
             canvas.height = image.height;
 
             context.drawImage(image, 0, 0);
+            // disable to not puth the image in grey scale
+            convertToGrayScales(context, canvas.width, canvas.height);
         }
-        image.src = reader.result;
+
+        image.src = reader.result as any;
     };
 
     reader.readAsDataURL(file);
+};
+
+function toGrayScale(red: number, green: number, blue: number) : number {
+    return 0.21 * red + 0.72 * green + 0.07 * blue;
+}
+
+function convertToGrayScales(context: CanvasRenderingContext2D, width: number, height: number) : number[] {
+    const imageData : ImageData = context.getImageData(0, 0, width, height);
+    const grayScales : number[] = [];
+
+    for (let i = 0 ; i < imageData.data.length ; i += 4) {
+        const red = imageData.data[i];
+        const green = imageData.data[i + 1];
+        const blue = imageData.data[i + 2];
+
+        const grayScale = toGrayScale(red, green, blue);
+        imageData.data[i] = imageData.data[i + 1] = imageData.data[i + 2] = grayScale;
+
+        grayScales.push(grayScale);
+    }
+
+    context.putImageData(imageData, 0, 0);
+
+    return grayScales;
 };
